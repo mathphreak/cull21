b6eb3349a5449d63c08f4e93bfc65c990c08a2a1
Matt Horn <matt@matthorn.tech>
2017-03-07 20:37:23 -0700

    Add and appease rubocop

--- spec/support/capybara_webkit.rb
+++ spec/support/capybara_webkit.rb
@@ -1,5 +1,5 @@
 Capybara.javascript_driver = :webkit
 
-Capybara::Webkit.configure do |config|
+Capybara::Webkit.configure do |config| # rubocop:disable Style/SymbolProc
   config.block_unknown_urls
 end
